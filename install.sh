42ae0f13b16a564cc6c6068a96dd533dfc2d7c6c
mahammad <msxiyev@gmail.com>
2016-09-15 17:21:12 +0400

    Added:install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#cd ~
+#wget https://raw.githubusercontent.com/mahammad/gg/master/gg.sh
+
+sudo cp gg.sh /usr/local/bin/gg
+sudo chmod +x /usr/local/bin/gg
+sudo chown -R root:$USER /usr/local/bin/gg
+#rm gg.sh
