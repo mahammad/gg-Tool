b1dbeb31e30c051a0f42dc8664f285ae33fb3a2c
mahammad <msxiyev@gmail.com>
2016-09-15 17:24:34 +0400

    Update: install.sh fix code

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-#cd ~
-#wget https://raw.githubusercontent.com/mahammad/gg/master/gg.sh
+cd ~
+wget https://raw.githubusercontent.com/mahammad/gg/master/gg.sh
 
 sudo cp gg.sh /usr/local/bin/gg
 sudo chmod +x /usr/local/bin/gg
 sudo chown -R root:$USER /usr/local/bin/gg
-#rm gg.sh
